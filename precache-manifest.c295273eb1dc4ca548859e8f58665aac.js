f54c0fb8ce06453213009bb34f8cd381a6913d0d
Ismail <esmael449@stud.fci-cu.edu.eg>
2020-06-21 00:20:23 +0200

    Updates

---
+++ precache-manifest.c295273eb1dc4ca548859e8f58665aac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b173bdde7941e9eb70ab58411d139868",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "7e587b8bdfc1da3c5d36",
+    "url": "/ravenous/static/css/main.41a19a5e.chunk.css"
+  },
+  {
+    "revision": "10126e3e925687b0b41e",
+    "url": "/ravenous/static/js/2.b152fa3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.b152fa3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e587b8bdfc1da3c5d36",
+    "url": "/ravenous/static/js/main.05b60033.chunk.js"
+  },
+  {
+    "revision": "39e841b39d02e0e6343f",
+    "url": "/ravenous/static/js/runtime-main.3824ce30.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
